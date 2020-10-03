6f145555d11b2b51fc4ad5bb5037485682fc8f8b
Irina <alkhoviki@mail.ru>
2020-10-03 11:45:25 +0300

    maked project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,77 @@
+' use strict';
 
 const menuIcon = document.querySelector('.menu-icon'),
       menu = document.querySelector('.menu'),
-      menuItem = document.querySelectorAll('.list-item');
+      menuItem = document.querySelectorAll('.list-item'),
+      prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
+      slides = document.querySelectorAll('.slide'),
+      dot = document.querySelectorAll('.dots');
+
+let index = 0;
+
+const activeSlide = n => {
+  for (slide of slides) {
+    slide.classList.remove('active');
+  }
+  slides[n].classList.add('active');
+};
+
+const activeDot = n => {
+  for(dots of dot) {
+    dots.classList.remove('active');
+  }
+  dot[n].classList.add('active');
+};
+
+const prepareCurrentSlide = ind => {
+  activeSlide(ind);
+  activeDot(ind);
+};
+
+const nextSlide = () => {
+  if(index == slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index == 0) {
+    index = slides.length - 1;
+    prepareCurrentSlide(index);
+  } else {
+    index--;
+    prepareCurrentSlide(index);
+  }
+}
+
+dot.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index= indexDot;
+    prepareCurrentSlide(index);
+  });
+});
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+/* const interval = setInterval(nextSlide, 2500);
+
+next.addEventListener('click', () => {
+  clearInterval(interval);
+}, false); */
+
+/* бургер-меню */
 
 function phoneMenu() {
+  menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('clicked');
+  });
+
   menuIcon.addEventListener('click', () => {
     menu.classList.toggle('open');
   });
@@ -14,9 +82,10 @@ function phoneMenu() {
       menuIcon.classList.toggle('change');
     });
   });
-};
-phoneMenu();
 
-$('.menu-icon').click(function(){
-  $(this).toggleClass('clicked');
-});
+  menuIcon.onclick = function() {
+    menu.style.display === 'block' ? menu.style.display = 'none' : menu.style.display = 'block';
+  };
+};
+
+phoneMenu();
